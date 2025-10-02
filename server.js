8a8638072d25af87a7efad7a584d00104d6d2c3d
greekaway <mersi.sakellariouoe@gmail.com>
2025-10-02 23:19:29 +0300

    06

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Σερβίρουμε όλα τα static αρχεία (html, css, js, εικόνες)
+app.use(express.static(path.join(__dirname)));
+
+// Ακούμε στο port 3000
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
